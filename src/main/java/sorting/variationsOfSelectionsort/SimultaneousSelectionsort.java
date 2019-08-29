08ffebd214ae2b8493db28d294218adf715c0874
WendsonMagalhaes <wendson.silva@ccc.ufcg.edu.br>
2019-08-29 11:24:01 -0300

    Primeiro commit

---
+++ src/main/java/sorting/variationsOfSelectionsort/SimultaneousSelectionsort.java
@@ -0,0 +1,43 @@
+package sorting.variationsOfSelectionsort;
+
+import sorting.AbstractSorting;
+import util.Util;
+
+/**
+ * This algorithm applies two selection sorts simultaneously. In a same
+ * iteration, a selection sort pushes the greatest elements to the right and
+ * another selection sort pushes the smallest elements to the left. At the end
+ * of the first iteration, the smallest element is in the first position (index
+ * 0) and the greatest element is the last position (index N-1). The next
+ * iteration does the same from index 1 to index N-2. And so on. The execution
+ * continues until the array is completely ordered.
+ */
+public class SimultaneousSelectionsort<T extends Comparable<T>> extends AbstractSorting<T> {
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		int n = 0;
+
+		for (int i = leftIndex; i < rightIndex - n; i++) {
+			int limit = rightIndex - n;
+			int min = i;
+
+			for (int j = i + 1; j <= limit; j++) {
+				if (array[j].compareTo(array[min]) < 0) {
+					min = j;
+				}
+			}
+
+			Util.swap(array, i, min);
+
+			int max = i + 1;
+
+			for (int j = max + 1; j <= limit; j++) {
+				if (array[j].compareTo(array[max]) > 0) {
+					max = j;
+				}
+			}
+
+			Util.swap(array, limit, max);
+			n++;
+		}
+	}
+}
